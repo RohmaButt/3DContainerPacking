3b4c1e26dad7a58f132f8aeb5a7fb22c802e3dfc
Rohma Butt <rohma.butt@afiniti.com>
2022-11-17 05:07:00 +0500

    Forked changes for Upowork Customer

--- src/CromulentBisgetti.DemoApp/Startup.cs
+++ src/CromulentBisgetti.DemoApp/Startup.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,10 +27,12 @@ namespace CromulentBisgetti.DemoApp
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
-                .AddJsonOptions(options =>
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .AddNewtonsoftJson(opt =>
                 {
-                    options.SerializerSettings.ContractResolver = null;
+                    opt.SerializerSettings.ContractResolver = null;
                 });
         }
 
